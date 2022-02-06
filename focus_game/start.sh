83496a4567713f778f1baf8cb31175c91635856a
Christopher Reighley <reighley@gmail.com>
2022-02-06 02:42:25 +0000

    oops horked start script

--- focus_game/start.sh
+++ focus_game/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CRPROPS=/home/reighley/Code/misc/focus_game/focus_game.properties
-/usr/bin/scala /home/reighley/Code/misc/focus_game/controlserver.jar /home/reighley/Code/misc/focus_game/controlserver.properties &
+export CRPROPS=/home/reighley/misc/focus_game/focus_game.properties
+/usr/bin/scala /home/reighley/misc/focus_game/controlserver.jar /home/reighley/misc/focus_game/controlserver.properties &
 echo $! > /var/run/focus_game.pid
